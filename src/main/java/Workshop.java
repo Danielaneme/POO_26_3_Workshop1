8374107fbde3c5fc86ba18580dbb61304076fc0a
Danielaneme <danipollo2004@gmail.com>
2026-09-21 19:47:42 -0600

    feat: implementar areaCirculo

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -1,14 +1,6 @@
-public String pptls2(String[] game) {
-    if (game == null || game.length < 2) return "";
-    String p1 = game[0].toLowerCase();
-    String p2 = game[1].toLowerCase();
-    if (p1.equals(p2)) return "Empate";
-    if ((p1.equals("tijera") && (p2.equals("papel") || p2.equals("lagarto"))) ||
-        (p1.equals("papel") && (p2.equals("piedra") || p2.equals("spock"))) ||
-        (p1.equals("piedra") && (p2.equals("lagarto") || p2.equals("tijera"))) ||
-        (p1.equals("lagarto") && (p2.equals("spock") || p2.equals("papel"))) ||
-        (p1.equals("spock") && (p2.equals("tijera") || p2.equals("piedra")))) {
-        return "Jugador 1";
+public double areaCirculo(double radio) {
+    if (radio < 0) {
+        throw new IllegalArgumentException("El radio no puede ser negativo");
     }
-    return "Jugador 2";
+    return Math.PI * radio * radio;
 }
