028d75fff696964f07586375532b5b20d558b242
Danielaneme <danipollo2004@gmail.com>
2026-09-21 19:43:26 -0600

    feat: implementar validarCorreoElectronico

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -1,4 +1,5 @@
-public int buscarSubcadena(String cadena, String subcadena) {
-    if (cadena == null || subcadena == null) return -1;
-    return cadena.indexOf(subcadena);
+public boolean validarCorreoElectronico(String correo) {
+    if (correo == null) return false;
+    String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+    return correo.matches(regex);
 }
