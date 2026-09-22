c9fb0c8af29ac7551653753e0b8a5ecb71982571
Danielaneme <danipollo2004@gmail.com>
2026-09-21 19:34:35 -0600

    feat: implementar ordenarArreglo

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -1,8 +1,6 @@
-public int[] invertirArreglo(int[] arreglo) {
+public int[] ordenarArreglo(int[] arreglo) {
     if (arreglo == null) return new int[0];
-    int[] invertido = new int[arreglo.length];
-    for (int i = 0; i < arreglo.length; i++) {
-        invertido[i] = arreglo[arreglo.length - 1 - i];
-    }
-    return invertido;
+    int[] ordenado = arreglo.clone();
+    java.util.Arrays.sort(ordenado);
+    return ordenado;
 }
