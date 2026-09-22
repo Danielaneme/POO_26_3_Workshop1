623fc88c109b30d9c28af506a3986cd8419eea7c
Danielaneme <danipollo2004@gmail.com>
2026-09-21 19:38:19 -0600

    feat: implementar contarCaracteres

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -1,11 +1,4 @@
-public int[] rotarArreglo(int[] arreglo, int posiciones) {
-    if (arreglo == null || arreglo.length == 0) return new int[0];
-    int n = arreglo.length;
-    posiciones = posiciones % n;
-    if (posiciones < 0) posiciones += n;
-    int[] rotado = new int[n];
-    for (int i = 0; i < n; i++) {
-        rotado[i] = arreglo[(i + posiciones) % n];
-    }
-    return rotado;
+public int contarCaracteres(String cadena) {
+    if (cadena == null) return 0;
+    return cadena.length();
 }
