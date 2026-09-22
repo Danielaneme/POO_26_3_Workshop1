3bdbcefc7a888918f75217abc8eeeea75fc6594f
Danielaneme <danipollo2004@gmail.com>
2026-09-21 19:39:26 -0600

    feat: implementar esPalindromo

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -1,4 +1,6 @@
-public String invertirCadena(String cadena) {
-    if (cadena == null) return "";
-    return new StringBuilder(cadena).reverse().toString();
+public boolean esPalindromo(String cadena) {
+    if (cadena == null) return false;
+    String limpia = cadena.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+    String invertida = new StringBuilder(limpia).reverse().toString();
+    return limpia.equals(invertida);
 }
