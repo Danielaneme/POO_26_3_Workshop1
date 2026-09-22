0b739b24ed83ea0d1f573f01673bd9730b664d1e
Danielaneme <danipollo2004@gmail.com>
2026-09-21 19:30:57 -0600

    feat: implementar sumaElementos

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -1,13 +1,8 @@
-public int[] serieFibonacci(int n) {
-    if (n < 0) {
-        throw new IllegalArgumentException("El número no puede ser negativo");
+public int sumaElementos(int[] arreglo) {
+    if (arreglo == null) return 0;
+    int suma = 0;
+    for (int num : arreglo) {
+        suma += num;
     }
-    if (n == 0) return new int[0];
-    int[] fib = new int[n];
-    if (n >= 1) fib[0] = 0;
-    if (n >= 2) fib[1] = 1;
-    for (int i = 2; i < n; i++) {
-        fib[i] = fib[i - 1] + fib[i - 2];
-    }
-    return fib;
+    return suma;
 }
