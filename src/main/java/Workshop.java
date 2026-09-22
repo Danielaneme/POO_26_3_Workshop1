a79f02701ec9f5829049d31e9f9b0102af100277
Danielaneme <danipollo2004@gmail.com>
2026-09-21 19:44:49 -0600

    feat: implementar convertirABinario

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -1,8 +1,3 @@
-public double promedioLista(java.util.List<Integer> lista) {
-    if (lista == null || lista.isEmpty()) return 0.0;
-    double suma = 0;
-    for (int num : lista) {
-        suma += num;
-    }
-    return suma / lista.size();
+public String convertirABinario(int numero) {
+    return Integer.toBinaryString(numero);
 }
